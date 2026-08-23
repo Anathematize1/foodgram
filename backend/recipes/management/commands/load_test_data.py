e87af7f6b0d952ba957a5d894b4d4a85af26c000
Vlad Rozum <vladrozum100302@gmail.com>
2026-08-23 15:28:34 +0300

    edit load_test_data and ci

--- backend/recipes/management/commands/load_test_data.py
+++ backend/recipes/management/commands/load_test_data.py
@@ -1,126 +1,266 @@
+"""Загрузка тегов, авторов и реальных рецептов с фотографиями."""
+from urllib.request import Request, urlopen
+
+from django.conf import settings
 from django.core.files.base import ContentFile
-from django.core.management.base import BaseCommand
+from django.core.management.base import BaseCommand, CommandError
+from django.db.models import Q
 
 from recipes.models import Ingredient, Recipe, RecipeIngredient, Tag
 from users.models import User
 
-PNG_1PX = (
-    b'\x89PNG\r\n\x1a\n\x00\x00\x00\rIHDR\x00\x00\x00\x01'
-    b'\x00\x00\x00\x01\x08\x02\x00\x00\x00\x90wS\xde\x00\x00'
-    b'\x00\x0cIDATx\x9cc\xf8\xcf\xc0\x00\x00\x00\x03\x00\x01'
-    b'\x00\x05\x18\xd8N\x00\x00\x00\x00IEND\xaeB`\x82'
-)
+RECIPE_IMAGES = {
+    'omelette.jpg': (
+        'https://commons.wikimedia.org/wiki/Special:FilePath/'
+        'Egg_omelette.JPG?width=1280'
+    ),
+    'borscht.jpg': (
+        'https://commons.wikimedia.org/wiki/Special:FilePath/'
+        'Borscht_served.jpg?width=1280'
+    ),
+    'carbonara.jpg': (
+        'https://commons.wikimedia.org/wiki/Special:FilePath/'
+        'Espaguetis_carbonara.jpg?width=1280'
+    ),
+    'syrniki.jpg': (
+        'https://commons.wikimedia.org/wiki/Special:FilePath/'
+        'Syrniki.jpg?width=1280'
+    ),
+    'greek_salad.jpg': (
+        'https://commons.wikimedia.org/wiki/Special:FilePath/'
+        'Horiatiki_salad.jpg?width=1280'
+    ),
+}
 
 
 class Command(BaseCommand):
-    """Создаёт тестовых пользователей, теги и рецепты."""
+    """Создаёт теги, авторов и рецепты с фотографиями."""
 
-    help = 'Создаёт теги, пользователей и тестовые рецепты'
+    help = 'Создаёт теги, пользователей и рецепты с реальными фото'
 
     def handle(self, *args, **options):
-        tags = {
-            'Завтрак': 'breakfast',
-            'Обед': 'lunch',
-            'Ужин': 'dinner',
-        }
-        tag_objects = []
-        for name, slug in tags.items():
-            tag, _ = Tag.objects.get_or_create(name=name, slug=slug)
-            tag_objects.append(tag)
+        tags = self._create_tags()
+        users = self._create_users()
+        if not self._ingredients_ready():
+            return
+        self._create_recipes(tags, users)
+        self.stdout.write(self.style.SUCCESS('Тестовые данные загружены.'))
 
+    def _create_tags(self):
+        tags = {}
+        for name, slug in (
+            ('Завтрак', 'breakfast'),
+            ('Обед', 'lunch'),
+            ('Ужин', 'dinner'),
+        ):
+            tag, _ = Tag.objects.get_or_create(name=name, slug=slug)
+            tags[slug] = tag
+        return tags
+
+    def _create_users(self):
         users_data = (
             {
-                'email': 'admin@foodgram.local',
-                'username': 'admin',
-                'first_name': 'Админ',
-                'last_name': 'Проекта',
-                'password': 'Admin123456',
-                'is_staff': True,
-                'is_superuser': True,
-            },
-            {
-                'email': 'vasya@foodgram.local',
-                'username': 'vasya',
-                'first_name': 'Вася',
-                'last_name': 'Пупкин',
+                'email': 'maria@foodgram.local',
+                'username': 'maria',
+                'first_name': 'Мария',
+                'last_name': 'Ковалева',
                 'password': 'Qwerty123',
             },
             {
-                'email': 'petya@foodgram.local',
-                'username': 'petya',
-                'first_name': 'Петя',
-                'last_name': 'Иванов',
+                'email': 'ivan@foodgram.local',
+                'username': 'ivan',
+                'first_name': 'Иван',
+                'last_name': 'Соколов',
+                'password': 'Qwerty123',
+            },
+            {
+                'email': 'olga@foodgram.local',
+                'username': 'olga',
+                'first_name': 'Ольга',
+                'last_name': 'Морозова',
                 'password': 'Qwerty123',
             },
         )
         users = []
         for data in users_data:
             password = data.pop('password')
-            user, created = User.objects.get_or_create(
-                email=data['email'],
-                defaults=data,
-            )
-            if created:
-                user.set_password(password)
-                user.save()
+            user = User.objects.filter(
+                Q(email=data['email']) | Q(username=data['username']),
+            ).first()
+            if user is None:
+                user = User.objects.create_user(password=password, **data)
             users.append(user)
+        return users
 
-        ingredients = list(Ingredient.objects.all()[:6])
-        if len(ingredients) < 2:
+    def _ingredients_ready(self):
+        if Ingredient.objects.count() < 10:
             self.stdout.write(
                 self.style.WARNING(
                     'Сначала выполните: python manage.py load_ingredients',
                 ),
             )
-            return
+            return False
+        return True
 
-        recipes_data = (
-            (
-                'Омлет',
-                'Простой омлет на завтрак.',
-                15,
-                users[0],
-                tag_objects[0],
-            ),
-            (
-                'Борщ',
-                'Классический борщ на обед.',
-                90,
-                users[1],
-                tag_objects[1],
-            ),
-            (
-                'Паста',
-                'Паста на ужин.',
-                25,
-                users[2],
-                tag_objects[2],
-            ),
+    def _get_ingredient(self, name):
+        ingredient = Ingredient.objects.filter(name=name).first()
+        if ingredient is None:
+            raise CommandError(f'Ингредиент не найден: {name}')
+        return ingredient
+
+    def _load_image(self, filename):
+        candidates = (
+            settings.BASE_DIR / 'data' / 'recipe_images' / filename,
+            settings.BASE_DIR.parent / 'data' / 'recipe_images' / filename,
         )
-        for name, text, time, author, tag in recipes_data:
+        for local_path in candidates:
+            if local_path.is_file():
+                return ContentFile(local_path.read_bytes(), name=filename)
+        url = RECIPE_IMAGES[filename]
+        request = Request(url, headers={'User-Agent': 'Foodgram/1.0'})
+        with urlopen(request, timeout=30) as response:
+            return ContentFile(response.read(), name=filename)
+
+    def _create_recipes(self, tags, users):
+        recipes = (
+            {
+                'name': 'Омлет с сыром',
+                'text': (
+                    'Взбейте яйца с молоком и щепоткой соли. Разогрейте '
+                    'сковородку с оливковым маслом, вылейте смесь и жарьте '
+                    'на среднем огне 3–4 минуты. Посыпьте тёртым сыром, '
+                    'сложите омлет пополам и сразу подавайте.'
+                ),
+                'cooking_time': 15,
+                'author': users[0],
+                'tags': (tags['breakfast'],),
+                'image': 'omelette.jpg',
+                'ingredients': (
+                    ('яйца куриные', 120),
+                    ('молоко', 50),
+                    ('сыр твердый', 40),
+                    ('соль', 3),
+                    ('оливковое масло', 10),
+                ),
+            },
+            {
+                'name': 'Сырники',
+                'text': (
+                    'Смешайте творог, яйцо, сахар и муку до однородного '
+                    'теста. Сформируйте небольшие лепёшки, обваляйте в муке '
+                    'и обжарьте на оливковом масле с двух сторон до '
+                    'золотистой корочки. Подавайте со сметаной.'
+                ),
+                'cooking_time': 25,
+                'author': users[0],
+                'tags': (tags['breakfast'],),
+                'image': 'syrniki.jpg',
+                'ingredients': (
+                    ('творог', 400),
+                    ('яйца куриные', 50),
+                    ('сахар', 30),
+                    ('мука', 60),
+                    ('сметана', 80),
+                    ('оливковое масло', 20),
+                ),
+            },
+            {
+                'name': 'Борщ',
+                'text': (
+                    'Отварите говядину до готовности и снимите пену. '
+                    'Свеклу натрите, обжарьте с томатной пастой. Картофель, '
+                    'капусту и лук добавьте в бульон, затем свеклу. '
+                    'Варите до мягкости овощей, в конце положите чеснок и '
+                    'укроп. Подавайте со сметаной.'
+                ),
+                'cooking_time': 90,
+                'author': users[1],
+                'tags': (tags['lunch'],),
+                'image': 'borscht.jpg',
+                'ingredients': (
+                    ('говядина', 400),
+                    ('свекла', 300),
+                    ('капуста белокочанная', 250),
+                    ('картофель', 300),
+                    ('лук репчатый', 100),
+                    ('томатная паста', 30),
+                    ('чеснок', 10),
+                    ('укроп', 10),
+                    ('сметана', 80),
+                    ('соль', 8),
+                ),
+            },
+            {
+                'name': 'Греческий салат',
+                'text': (
+                    'Крупно нарежьте помидоры, огурцы и лук. Добавьте '
+                    'оливки и кубики феты. Заправьте оливковым маслом, '
+                    'лимонным соком, солью и чёрным перцем. Аккуратно '
+                    'перемешайте и сразу подавайте.'
+                ),
+                'cooking_time': 15,
+                'author': users[2],
+                'tags': (tags['lunch'],),
+                'image': 'greek_salad.jpg',
+                'ingredients': (
+                    ('помидоры', 250),
+                    ('огурцы', 200),
+                    ('лук репчатый', 50),
+                    ('оливки', 60),
+                    ('фета', 120),
+                    ('оливковое масло', 30),
+                    ('лимонный сок', 15),
+                    ('соль', 3),
+                    ('перец черный молотый', 2),
+                ),
+            },
+            {
+                'name': 'Паста карбонара',
+                'text': (
+                    'Отварите спагетти в подсоленной воде до состояния '
+                    'аль денте. Бекон обжарьте до хруста. Смешайте яйца '
+                    'с тёртым сыром. Слейте пасту, сразу соедините с беконом '
+                    'и яично-сырной смесью, активно перемешивая. Подавайте '
+                    'с чёрным перцем.'
+                ),
+                'cooking_time': 25,
+                'author': users[1],
+                'tags': (tags['dinner'],),
+                'image': 'carbonara.jpg',
+                'ingredients': (
+                    ('спагетти', 250),
+                    ('бекон', 120),
+                    ('яйца куриные', 100),
+                    ('сыр твердый', 80),
+                    ('соль', 5),
+                    ('перец черный молотый', 3),
+                ),
+            },
+        )
+        for data in recipes:
             recipe, created = Recipe.objects.get_or_create(
-                name=name,
-                author=author,
+                name=data['name'],
+                author=data['author'],
                 defaults={
-                    'text': text,
-                    'cooking_time': time,
+                    'text': data['text'],
+                    'cooking_time': data['cooking_time'],
                 },
             )
-            if created:
-                recipe.image.save(
-                    f'{recipe.short_code}.png',
-                    ContentFile(PNG_1PX),
-                    save=True,
+            if not created:
+                continue
+            try:
+                image = self._load_image(data['image'])
+            except OSError as exc:
+                recipe.delete()
+                raise CommandError(
+                    f'Не удалось загрузить фото {data["image"]}: {exc}',
                 )
-                recipe.tags.add(tag)
+            recipe.image.save(data['image'], image, save=True)
+            recipe.tags.set(data['tags'])
+            for ingredient_name, amount in data['ingredients']:
                 RecipeIngredient.objects.create(
                     recipe=recipe,
-                    ingredient=ingredients[0],
-                    amount=2,
+                    ingredient=self._get_ingredient(ingredient_name),
+                    amount=amount,
                 )
-                RecipeIngredient.objects.create(
-                    recipe=recipe,
-                    ingredient=ingredients[1],
-                    amount=100,
-                )
-        self.stdout.write(self.style.SUCCESS('Тестовые данные загружены.'))
+            self.stdout.write(f'Создан рецепт: {recipe.name}')
